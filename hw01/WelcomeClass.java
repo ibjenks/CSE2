16fde241035a10bbbbe33214428ae81f05897edc
ibjenks <tdj218@lehigh.edu>
2014-08-29 15:59:00 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+// Tyler Jenkins 
+// 8/29/14
+// CSE2
+// Welcome to Class Java Application
+//
+// First compile the program
+//      javac WelcomeClass
+// Then run the program
+//      java WelcomeClass 
+
+//  define a class
+    public class WelcomeClass{
+        
+//  add main method
+    public static void main(String[ ]  args){
+        
+// print the statement/picture
+    System.out.println("    -----------");
+    System.out.println("    | WELCOME |");
+    System.out.println("    -----------");
+    System.out.println(" ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-T--D--J--2--1--8-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v ");
+    System.out.println(" I play basketball here at Lehigh and I am from Indiana.");
+   
+}
+    
+    }
+    
